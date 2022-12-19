a03d9db49cde97c30b90f459d1fa47e2e0473bd3
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-12-19 18:45:22 -0300

    Feat.: Aula 2 - Finalizada

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -2,15 +2,15 @@ import { Content } from './content';
 
 describe('Notification content', () => {
   it('should be able to create a notification content', () => {
-    const content = new Content('Você recebeu uma nova solicitação');
+    const content = new Content('You received a new notification');
     expect(content).toBeTruthy;
   });
 
   it('should not be able to create a notification content with less than 5 characters', () => {
-    expect(() => new Content('Você')).toThrow;
+    expect(() => new Content('You')).toThrow;
   });
 
   it('should not be able to create a notification content with more than 240 characters', () => {
-    expect(() => new Content('V'.repeat(241))).toThrow;
+    expect(() => new Content('Y'.repeat(241))).toThrow;
   });
 });
--- src/application/entities/notification.spec.ts
+++ src/application/entities/notification.spec.ts
@@ -4,7 +4,7 @@ import { Notification } from './notification';
 describe('Notification', () => {
   it('should be able to create a notification content', () => {
     const notification = new Notification({
-      content: new Content('Nova notificação'),
+      content: new Content('New notification'),
       category: 'social',
       recipientId: 'example-recipient-id',
     });
